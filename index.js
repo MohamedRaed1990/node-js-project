ea0498d70e326a3936d373c7bc8ee3929013db09
MohamedRaed1990 <113235785+MohamedRaed1990@users.noreply.github.com>
2025-09-01 02:16:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ app.patch("/article/:articleID",async(req,res)=>{
 
 app.get("/",async(req,res)=>{
     const articles = await Article.find();
-    res.render("articles.ejs",{
+    res.render("articles",{
         allArticles : articles
     });
 })
